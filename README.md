fc8d213dbbc65c230570cc0a5e0a213ba85ffb3b
diogenes25 <tjark@onnen.de>
2019-06-07 13:46:31 +0200

    3 files modified, 3 added

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # twiddler3
-
+Das ist Hammer!
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/twiddler3)
---
+++ src/app/keybord/keybord.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'keybord',
+  templateUrl: './keybord.component.html',
+  styleUrls: ['./keybord.component.css']
+})
+export class KeybordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
